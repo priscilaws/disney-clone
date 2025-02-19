2e9af935b42b1e04e4418a6d67b1e4df078fa18a
Priscila <pri.wagner.silva@gmail.com>
2025-02-19 14:47:53 +0000

    changed file

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,29 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
 
+// Compile and minify SCSS
 function styles() {
-    return gulp.src('./src/styles/*.scss')
-        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError)) // Added error handling
+    return gulp.src('./src/styles/**/*.scss') // Use '**/*.scss' to include subdirectories
+        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
         .pipe(gulp.dest('./dist/css'));
 }
 
-// Watch task to automatically recompile SCSS on file changes
-function watchFiles() {
-    gulp.watch('./src/styles/**/*.scss', styles);
+// Optimize images
+function images() {
+    return gulp.src('./src/images/**/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/images'));
 }
 
-exports.default = styles;
+// Watch for changes in SCSS and images
+function watchFiles() {
+    gulp.watch('./src/styles/**/*.scss', styles);
+    gulp.watch('./src/images/**/*', images); // Watch for image changes
+}
+
+// Default task runs both styles and images
+exports.default = gulp.parallel(styles, images);
 exports.watch = watchFiles;
+exports.styles = styles;
+exports.images = images;
