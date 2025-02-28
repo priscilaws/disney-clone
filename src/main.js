9530ea2767d7ea689a2c4b689340754ba3dd1370
Priscila <pri.wagner.silva@gmail.com>
2025-02-28 23:01:24 +0000

    added faq section

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 document.addEventListener ('DOMContentLoaded', function () {
     const buttons = document.querySelectorAll('[data-tab-button]');
+    const questions = document.querySelectorAll('[data-faq-question]')
 
     console.log(buttons)
 
@@ -13,9 +14,20 @@ document.addEventListener ('DOMContentLoaded', function () {
             event.target.classList.add('shows__tabs__button--is-active');
         })
     }
+
+    for (let i = 0; i < questions.length; i++) {
+        questions[i].addEventListener('click', abreOuFechaResposta);
+    }
 })
 
-function removeBotaoAtivo() {
+function abreOuFechaResposta(elemento) {
+    const classe = 'faq__questions__item--is-open';
+    const elemntoPai = elemento.target.parentNode;
+
+    elemntoPai.classList.toggle(classe);
+}
+
+function removeBotaoAtivo(elemento) {
     const buttons = document.querySelectorAll('[data-tab-button]');
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].classList.remove('shows__tabs__button--is-active');
